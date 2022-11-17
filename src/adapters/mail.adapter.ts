5b09832caeaa61a0ed0dd890c9c78941e51c11dc
Nickolay <arbuzov.n@f-case.ru>
2022-11-18 00:11:47 +0300

    feat

--- src/adapters/mail.adapter.ts
+++ src/adapters/mail.adapter.ts
@@ -7,8 +7,8 @@ export const sendEmail = async (email: string, code: string) => {
     host: 'smtp.gmail.com',
     port: 465,
     auth: {
-      user: 'nickarb10888@gmail.com',
-      pass: 'treecyvaqtxnhmzs',
+      user: process.env.GMAIL_ACCAUNT,
+      pass: process.env.GMAIL_PASSWORD,
     }
   })
 
@@ -18,10 +18,10 @@ export const sendEmail = async (email: string, code: string) => {
      </p>`
  
   await transporter.sendMail({
-    from: 'nickarb10888@gmail.com',
+    from: process.env.GMAIL_ACCAUNT,
     to: 'nickarbuzov@yandex.by',
     html: emailTemplate(code),
-    subject: 'Регистрация',
+    subject: 'Registration vercel',
   })
 
 }
